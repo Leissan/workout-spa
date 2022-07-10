cd5af35a23b5c450953de37b20f68594b0822d39
Leissan Khan <leissan.khan@gmail.com>
2022-07-09 21:58:29 -0400

    newp

--- src/components/post/NewPost.js
+++ src/components/post/NewPost.js
@@ -2,6 +2,26 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { Layout } from '../layout';
 
+const NewPost = () => {
+  const [title, setTitle] = useState(null)
+  const [body, setBody] = useState(null)
+  const [author, setAuthor] = useState(null)
 
+  const onCreate = () => {
+      axios.post("http://localhost:4000/posts", {
+          "title": title, 
+          "body": body,
+          "author": author,
+          "createdAt": new Date()
+        }).then(() => {
+          setTitle(null)
+          setAuthor(null)
+          setBody(null)
+      })
+  }
+
+  return (
+  )
+}
 
 export default NewPost;
