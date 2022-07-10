63229d23b36080ffef0d7d47e47d670cf97c0e39
Leissan Khan <leissan.khan@gmail.com>
2022-07-09 22:07:21 -0400

    m

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Posts from './components/post/index';
+import NewPost from './components/post/NewPost';
+import { Post } from './components/post/Post';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="list_of_posts" element={<Posts />} />
+        <Route path="new_post" element={<NewPost />} />
+        <Route path="posts/${id}" element={<Post />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
