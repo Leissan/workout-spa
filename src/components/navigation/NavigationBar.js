9626c74e078617c1f1b132155e247c5f50fe7dc0
Leissan Khan <leissan.khan@gmail.com>
2022-07-09 22:03:23 -0400

    navb

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -1,42 +1,32 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container'
-import {Link} from 'react-router-dom'
+import { createUseStyles } from 'react-jss';
+import { Link } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+const useStyles = createUseStyles({
+    wrapper: {
+        display: "flex",
+        width: "100%",
+        margin: 10,
+        justifyContent: "center",
+    }
+  });
 
-const Navbar = () => {
-  const classes = useStyles();
+const NavigationBar = () => {
+    const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Container>
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-          <Button color="inherit" component ={Link} to = "/">Workouts</Button>
-          </Typography>
-          <Button color="inherit" component ={Link} to = "/workouts" >List Workouts</Button>
-          <Button color="inherit" component ={Link} to = "/workouts/new" >Create Workouts</Button>
-        </Toolbar>
-        </Container>
-      </AppBar>
-    </div>
-  );
+    return (
+        <div className={classes.wrapper}> 
+            <nav
+                style={{
+                    borderBottom: "solid 1px",
+                    paddingBottom: "1rem",
+                }}
+            >
+                <Link to="/list_of_posts">Posts list</Link> |{" "}
+                <Link to="/new_post">New post</Link>
+            </nav>
+        </div>
+    )
 }
 
-  export default Navbar;
+export default NavigationBar;
