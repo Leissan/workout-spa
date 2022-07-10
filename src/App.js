a0baec8ac3536e03dc78069fd85a04a9dba86897
Leissan Khan <leissan.khan@gmail.com>
2022-07-09 20:41:00 -0400

    clean

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>This is Leissan</h1>
    
     </div>
   );
