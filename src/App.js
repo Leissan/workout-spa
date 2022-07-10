e1831b562ec08dce0eecf2f92188bed034c10e36
Leissan Khan <leissan.khan@gmail.com>
2022-07-09 20:20:14 -0400

    now

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edittt <code>src/App.js</code> and save to reload.
+          Editttnow <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
