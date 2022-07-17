23a088068e03fe33b6db188096d55f3206ff11ba
Leissan Khan <leissan.khan@gmail.com>
2022-07-17 14:35:00 -0400

    key

--- src/components/post/Posts.js
+++ src/components/post/Posts.js
@@ -9,19 +9,26 @@ const Posts = () => {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        axios.get("http://localhost:4000/posts").then((response) => { 
+
+        axios.get("http://localhost:3000/posts").then((response) => { 
             setPosts(response.data)
         });
     }, []);
+
+    // useEffect(() => {
+    //     fetch ('http://localhost:3000/posts')
+    //     .then (res => res.json() )
+    //     .then (data => setPosts(response.data))
+    //   },[])
 
     return (
         <Layout>
             <main style={{ padding: "1rem 0"}}>
                 <h2>Workout Plan 💪 </h2>
                 { posts.length > 0 && posts.map((post) => (
-                    <div key={post.id}>
-                        <Post id={post.id} workout={post.workout} description={post.description}  repetitions={post.repetitions}/>
-                    </div>
+                    
+                        <Post key={post.id} id={post.id} workout={post.workout} description={post.description}  repetitions={post.repetitions}/>
+                    
                     ))}
             </main>
         </Layout>
